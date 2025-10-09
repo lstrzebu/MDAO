3a317937d3f34c11ce50101ac09ceea913b0b2be
lstrzebu <lstrzebu@ncsu.edu>
2025-10-09 14:38:38 -0400

    change score evaluation to be elementwise (evaluate multiple mission scores at once)

--- mission/evaluateScore.m
+++ mission/evaluateScore.m
@@ -1,4 +1,4 @@
-function recip = missionObjective(missionVars)
+function recip = evaluateScore(missionVars)
 % Liam Trzebunia
 % 26 September 2025
 % Calculate total competition score for an AIAA DBF 2025-2026 aircraft
@@ -14,19 +14,19 @@ fprintf('Calculating mission scores for designs... ')
 % variables.
 global b 
 global probabilities 
-p       = missionVars(1);
-c       = missionVars(2);
-l       = missionVars(3);
-bl      = missionVars(4);
-TPBC    = missionVars(5);
+p       = missionVars(:, 1);
+c       = missionVars(:, 2);
+l       = missionVars(:, 3);
+bl      = missionVars(:, 4);
+TPBC    = missionVars(:, 5);
 
 readM2pars
 
 RAC = 0.05*b + 0.75;
-EF = TPBC/100;
+EF = TPBC./100;
 
 successfulMission = 1; % points received for a successful Mission 1
-M1 = probabilities.M1 * successfulMission;
+M1 = probabilities.M1 .* successfulMission;
 
 global mission_time
 
@@ -34,12 +34,12 @@ global income_net_best
 global quantity_best
 global mission_time_best
 
-income = p*(Ip1 + Ip2*l) + c*(Ic1 + Ic2*l);
-cost = EF*l*(Ce + p*Cp + c*Cc);
+income = p.*(Ip1 + Ip2.*l) + c.*(Ic1 + Ic2.*l);
+cost = EF.*l.*(Ce + p.*Cp + c.*Cc);
 income_net = income - cost;
 M2 = 1 + income_net./income_net_best;
 
-quantity = (l*bl)/RAC;
+quantity = (l.*bl)./RAC;
 M3 = 2 + quantity./quantity_best;
 
 GM = mission_time_best/mission_time; 
@@ -52,12 +52,12 @@ techInspScore = 1; % assume we pass tech inspection
 flightAttempt = 1; % assume we attempt a flight
 
 TMS = M1 + M2 + M3 + GM;
-TRS = 0.15*proposal + 0.85*report; 
+TRS = 0.15.*proposal + 0.85.*report; 
 P = flyoff + techInspScore + flightAttempt;
 
-total_score = TMS*TRS + P;
+total_score = TMS.*TRS + P;
 
-recip = 1/total_score;
+recip = 1./total_score;
 
 fprintf('Done \n');
 
